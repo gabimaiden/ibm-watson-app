55c35e3d94e839081cc1bd4a050826eee14e01b2
Nobody <gabimaiden@icloud.com>
2020-11-03 22:18:00 -0300

    Update yml

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const { response } = require('express');
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const routes = require('./routes/routes')
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const corsOptions = {
+  methods: "GET,PUT,POST,DELETE,OPTIONS",
+  origin: "*",
+  allowedHeaders: ["Content-Type", "Authorization", "access_token", "ACCESS_TOKEN"],
+  exposedHeaders: ["Content-Type", "Authorization", "access_token", "ACCESS_TOKEN"],
+};
 
-app.use('/api/v1', routes);
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(express.static(__dirname + "/public"));
+
+const routes = require("./routes/routes");
+app.use("/api/v1", routes);
 
 module.exports = app;
